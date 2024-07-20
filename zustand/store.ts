2abbbabdbaca58e1fb1cc8919d0c8152c8673b3e
oleonardodeandrade <79455799+oleonardodeandrade@users.noreply.github.com>
2024-07-20 18:27:24 -0300

    feat: zustand

---
+++ zustand/store.ts
@@ -0,0 +1,25 @@
+// store.ts
+import { create } from 'zustand';
+import { User } from 'firebase/auth';
+
+interface Session {
+  id: string;
+  name: string;
+  dailyGoal: number;
+}
+
+interface StoreState {
+  user: User | null;
+  setUser: (user: User | null) => void;
+  sessions: Session[];
+  setSessions: (sessions: Session[]) => void;
+}
+
+const useStore = create<StoreState>(set => ({
+  user: null,
+  setUser: (user) => set({ user }),
+  sessions: [],
+  setSessions: (sessions) => set({ sessions }),
+}));
+
+export default useStore;
